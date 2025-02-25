f53a695ad384a322d83fb5454b4343c9a0c587cf
pamela-monteiro <plorenams@hotmail.com>
2025-02-25 18:01:08 +0000

    Iniciando

--- logicaSuperTrunfo.c
+++ logicaSuperTrunfo.c
@@ -6,6 +6,74 @@
 // Siga os comentários para implementar cada parte do desafio.
 
 int main() {
+
+
+     // Estado 1
+     char estado1[3];  
+     char codigo1[10];
+     char cidade1[50];
+     int populacao1;
+     float area1;
+     float pib1;
+     int pontos_turisticos1;
+ 
+     // Estado 2
+     char estado2[3];
+     char codigo2[10];
+     char cidade2[50];
+     int populacao2;
+     float area2;
+     float pib2;
+     int pontos_turisticos2;
+ 
+     // Entrada de dados primeiro estado
+     printf("Digite o estado:\n");
+     scanf("%s", estado1);
+ 
+     printf("Digite o codigo da cidade:\n");
+     scanf("%s",codigo1);
+ 
+     printf("Digite o nome da cidade:\n");
+     scanf("%s", cidade1);
+ 
+     printf("Digite a População:\n");
+     scanf("%d", &populacao1);
+ 
+     printf("Digite a área:\n");
+     scanf("%f", &area1);
+ 
+     printf("Digite o PIB:\n");
+     scanf("%f", &pib1);
+ 
+     printf("Digite o numero de Pontos Turisticos:\n");
+     scanf("%d", &pontos_turisticos1);
+ 
+     
+       // Entrada de dados estado2
+ 
+       printf("Digite o estado:\n");
+       scanf("%s", estado2);
+   
+       printf("Digite o codigo da cidade:\n");
+       scanf("%s", codigo2);
+   
+       printf("Digite o nome da cidade:\n");
+       scanf("%s", cidade2);
+   
+       printf("Digite a População:\n");
+       scanf("%d", &populacao2);
+   
+       printf("Digite a área:\n");
+       scanf("%f", &area2);
+   
+       printf("Digite o PIB:\n");
+       scanf("%f", &pib2);
+   
+       printf("Digite o numero de Pontos Turisticos:\n");
+       scanf("%d", &pontos_turisticos2);
+
+
+       
     // Definição das variáveis para armazenar as propriedades das cidades
     // Você pode utilizar o código do primeiro desafio
 
